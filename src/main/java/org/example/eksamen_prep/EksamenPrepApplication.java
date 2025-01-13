98cef6cfba1b8f6e2da758c89aa1b91d962b552a
kasperkhansen <kasper1999.hansen@gmail.com>
2025-01-13 16:54:43 +0100

    innit

---
+++ src/main/java/org/example/eksamen_prep/EksamenPrepApplication.java
@@ -0,0 +1,13 @@
+package org.example.eksamen_prep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksamenPrepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EksamenPrepApplication.class, args);
+    }
+
+}
